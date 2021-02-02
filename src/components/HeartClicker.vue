3d2e99191538e8526f5eb825e4a646894e9a9784
Simas <simas.raudonis@gmail.com>
2021-02-02 18:03:14 +0200

    project baseline

---
+++ src/components/HeartClicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <canvas
+        id="heart"
+        :width="width"
+        :height="height"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            width: 150,
+            height: 150
+        }
+    },
+    mounted() {
+    const canvas = this.$el.querySelector('#heart');
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(75, 40);
+        ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+        ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+        ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+        ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+        ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+        ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+        ctx.fillStyle = '#FF0000'; // heart color
+        ctx.fill(); // fill the shape we draw
+        }
+    }
+}
+</script>
